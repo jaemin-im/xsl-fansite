508cb89e90ffd954eee8783752822f1c7b690b20
Jaemin Lim <xsolute01@gmail.com>
2020-02-17 18:04:10 +0900

    Update calculate features

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,31 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
+      <v-spacer />
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-icon>mdi-arrow-all</v-icon>
+        <span class="ml-1 hidden-sm-and-down">DanceDanceRevolution Scroll Speed Checker</span>
+        <span class="ml-1 hidden-md-and-up">DDR Scroll Speed Checker</span>
       </v-btn>
+      <v-spacer />
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <ScrollCalculateForm />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ScrollCalculateForm from './components/ScrollCalculateForm.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    ScrollCalculateForm,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
---
+++ src/components/ScrollCalculateForm.vue
@@ -0,0 +1,138 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <v-text-field type="number" v-model="myScrollBpm" label="본인의 스크롤 속도" outlined />
+      </v-col>
+    </v-row>
+    <v-row class="mt-n8">
+      <v-col>
+        <v-text-field type="number" v-model="songBpm" label="곡의 BPM" outlined />
+      </v-col>
+    </v-row>
+    <v-row class="mt-n12">
+      <v-col class="d-flex justify-center">
+        <v-switch v-model="isPremium" inset :label="`프리미엄 모드 여부: ${isPremium}`" />
+      </v-col>
+    </v-row>
+    <!-- <v-row class="mt-n6">
+      <v-col class="d-flex justify-center">
+        <v-btn tile block outlined color="primary" @click="calculateSpeed">속도 계산</v-btn>
+      </v-col>
+    </v-row> -->
+    <v-row class="mt-n4">
+      <v-col class="d-flex justify-center mb-1">
+        <v-progress-circular
+          :size="200"
+          :width="30"
+          color="red lighten-1"
+          :value="slowMul / 0.25 * 3.125"
+        >
+          <span class="text-center" v-show="slowMul">
+            x{{ slowMul }}<br/>{{ slowBpmCal }} ({{ Math.abs(myScrollBpm) + Number(slowBpmCal) }})
+          </span>
+        </v-progress-circular>
+      </v-col>
+      <v-col class="d-flex justify-center">
+        <v-progress-circular
+          :size="200"
+          :width="30"
+          color="blue darken-1"
+          :value="fastMul / 0.25 * 3.125"
+        >
+          <span class="text-center" v-show="fastMul">
+            x{{ fastMul }}<br/>{{ fastBpmCal }} ({{ Math.abs(myScrollBpm) + Number(fastBpmCal) }})
+          </span>
+        </v-progress-circular>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+  name: 'ScrollCalculateForm',
+  data: () => ({
+    isPremium: false,
+    myScrollBpm: null,
+    songBpm: null,
+    fastMul: null,
+    fastBpmCal: null,
+    slowMul: null,
+    slowBpmCal: null,
+  }),
+  watch: {
+    isPremium() {
+      this.calculateSpeed();
+    },
+    myScrollBpm() {
+      this.calculateSpeed();
+    },
+    songBpm() {
+      this.calculateSpeed();
+    },
+  },
+  methods: {
+    async calculateSpeed() {
+      if (this.isPremium) {
+        const tempSlow = Math.floor((this.myScrollBpm / this.songBpm) * 4);
+        const tempFast = Math.ceil((this.myScrollBpm / this.songBpm) * 4);
+        if (tempSlow <= 1) {
+          this.slowMul = 0.25;
+        } else if (tempSlow >= 16 && tempSlow < 32) {
+          this.slowMul = Math.floor((this.myScrollBpm / this.songBpm) * 2) / 2;
+        } else if (tempSlow >= 32) {
+          this.slowMul = 8.0;
+        } else {
+          this.slowMul = Math.floor((this.myScrollBpm / this.songBpm) * 4) / 4;
+        }
+        if (tempFast <= 1) {
+          this.fastMul = 0.25;
+        } else if (tempFast >= 16 && tempFast < 32) {
+          this.fastMul = Math.ceil((this.myScrollBpm / this.songBpm) * 2) / 2;
+        } else if (tempFast >= 32) {
+          this.fastMul = 8.0;
+        } else {
+          this.fastMul = Math.ceil((this.myScrollBpm / this.songBpm) * 4) / 4;
+        }
+      } else {
+        const tempSlow = Math.floor((this.myScrollBpm / this.songBpm) * 2);
+        const tempFast = Math.ceil((this.myScrollBpm / this.songBpm) * 2);
+        if (tempSlow <= 2) {
+          this.slowMul = 1.0;
+        } else if (tempSlow >= 16) {
+          this.slowMul = 8.0;
+        } else {
+          this.slowMul = Math.floor((this.myScrollBpm / this.songBpm) * 2) / 2;
+        }
+        if (tempFast <= 2) {
+          this.fastMul = 1.0;
+        } else if (tempFast >= 16) {
+          this.fastMul = 8.0;
+        } else {
+          this.fastMul = Math.ceil((this.myScrollBpm / this.songBpm) * 2) / 2;
+        }
+      }
+      this.slowBpmCal = this.slowMul * this.songBpm - this.myScrollBpm;
+      this.fastBpmCal = this.fastMul * this.songBpm - this.myScrollBpm;
+
+      // await axios.get(`https://wx2hjouhrd.execute-api.ap-northeast-2.amazonaws.com/dev/${this.myScrollBpm}/${this.songBpm}/${this.isPremium}`)
+      //   .then((response) => {
+      //     if (response.data.result.fast.speedMul < response.data.result.slow.speedMul) {
+      //       this.fastMul = response.data.result.fast.speedMul;
+      //       this.slowMul = response.data.result.slow.speedMul;
+      //       this.fastBpmCal = response.data.result.fast.bpmCal;
+      //       this.slowBpmCal = response.data.result.slow.bpmCal;
+      //     } else {
+      //       this.fastMul = response.data.result.slow.speedMul;
+      //       this.slowMul = response.data.result.fast.speedMul;
+      //       this.fastBpmCal = response.data.result.slow.bpmCal;
+      //       this.slowBpmCal = response.data.result.fast.bpmCal;
+      //     }
+      //   });
+    },
+  },
+};
+</script>
