8523e109054513d3832a50da690897bd2d8cf713
Jaemin Lim <xsolute01@gmail.com>
2020-05-26 17:28:50 +0900

    chore: remove unused code (app bar)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    > -->
     <AppBar />
-      <!-- <v-spacer />
-      <v-btn
-        text
-      >
-        <v-icon>mdi-arrow-all</v-icon>
-        <span class="ml-1 hidden-sm-and-down">DanceDanceRevolution Scroll Speed Checker</span>
-        <span class="ml-1 hidden-md-and-up">DDR Scroll Speed Checker</span>
-      </v-btn>
-      <v-spacer />
-    </v-app-bar> -->
-
     <v-content>
       <ScrollCalculateForm />
     </v-content>
