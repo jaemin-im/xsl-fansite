320f84c70f11801971450095b33ba40f9a83c4f5
Jaemin Lim <xsolute01@gmail.com>
2020-05-26 17:58:32 +0900

    feat: add skill attack search component

---
+++ src/components/SkillAttackSearch.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row class="text-center">
+    <v-col>
+      <v-text-field type="number" v-model="ddrCode" label="DDR Code 입력 (예: 51551868)" outlined />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'SkillAttackSearch',
+  data: () => ({
+    ddrCode: null,
+  }),
+  // https://qog6l34nui.execute-api.ap-northeast-2.amazonaws.com/production/api/ddr_code/51551868
+};
+</script>
